c0df5064c128813886bacae90aae709f8573fc3d
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-16 04:02:08 +0700

    lua/custom: add options in custom dir
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

---
+++ lua/custom/plugins/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+-- Set cursor to block on all modes
+opt.guicursor = 'n-v-i-c:block-Cursor'
+
+-- Visual characters.
+opt.fillchars = {
+  eob = ' ',
+  vert = '│',
+  fold = ' ',
+  diff = '╱',
+}
+
+opt.list = true
+opt.listchars = {
+  tab = '  ',
+  trail = '·',
+  extends = '›',
+  precedes = '‹',
+  nbsp = '␣',
+}
