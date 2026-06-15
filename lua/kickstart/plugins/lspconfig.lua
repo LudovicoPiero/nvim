36b92bc2889690e9484e5f497b9a755a3d211082
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-16 04:37:53 +0700

    lua/kickstart: change lspconfig configuration
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -1,4 +1,7 @@
-local function gh(repo) return 'https://github.com/' .. repo end
+---@diagnostic disable: unresolved-require, type-not-found, assign-type-mismatch
+local function gh(repo)
+  return 'https://github.com/' .. repo
+end
 
 -- [[ LSP Configuration ]]
 -- Brief aside: **What is LSP?**
@@ -36,13 +39,13 @@ require('fidget').setup {}
 --    function will be executed to configure the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-  callback = function(event)
+  callback = function (event)
     -- NOTE: Remember that Lua is a real programming language, and as such it is possible
     -- to define small helper and utility functions so you don't have to repeat yourself.
     --
     -- In this case, we create a function that lets us more easily define mappings specific
     -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local map = function(keys, func, desc, mode)
+    local map = function (keys, func, desc, mode)
       mode = mode or 'n'
       vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
@@ -70,21 +73,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
         buffer = event.buf,
         group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
+        callback = vim.lsp.buf.document_highlight
       })
 
       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
         buffer = event.buf,
         group = highlight_augroup,
-        callback = vim.lsp.buf.clear_references,
+        callback = vim.lsp.buf.clear_references
       })
 
       vim.api.nvim_create_autocmd('LspDetach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-        callback = function(event2)
+        callback = function (event2)
           vim.lsp.buf.clear_references()
           vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-        end,
+        end
       })
     end
 
@@ -93,9 +96,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --
     -- This may be unwanted, since they displace some of your code
     if client and client:supports_method('textDocument/inlayHint', event.buf) then
-      map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+      map('<leader>th', function ()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+      end, '[T]oggle Inlay [H]ints'
+      )
     end
-  end,
+  end
 })
 
 -- Enable the following language servers
@@ -103,59 +109,41 @@ vim.api.nvim_create_autocmd('LspAttach', {
 --  See `:help lsp-config` for information about keys and how to configure
 ---@type table<string, vim.lsp.Config>
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  --
+  -- C/C++
+  clangd = {},
+  ['clang-format'] = {},
+
+  -- Golang
+  gopls = {},
+  gofumpt = {},
+  goimports = {},
+
+  -- Python
+  basedpyright = {},
+  ruff = {},
+
+  -- Rust
+  rust_analyzer = {},
+
+  -- -- Nix
+  -- ['nil'] = {},
+  -- nixfmt = {},
+  -- statix = {},
+
   -- Some languages (like typescript) have entire language plugins that can be useful:
   --    https://github.com/pmizio/typescript-tools.nvim
   --
   -- But for many setups, the LSP (`ts_ls`) will work just fine
-  -- ts_ls = {},
-
-  stylua = {}, -- Used to format Lua code
+  ts_ls = {},
 
   -- Special Lua Config, as recommended by neovim help docs
-  lua_ls = {
-    on_init = function(client)
-      client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
-
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-      end
-
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          version = 'LuaJIT',
-          path = { 'lua/?.lua', 'lua/?/init.lua' },
-        },
-        workspace = {
-          checkThirdParty = false,
-          -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-          --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-          library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-            '${3rd}/luv/library',
-            '${3rd}/busted/library',
-          }),
-        },
-      })
-    end,
-    ---@type lspconfig.settings.lua_ls
-    settings = {
-      Lua = {
-        format = { enable = false }, -- Disable formatting (formatting is done by stylua)
-      },
-    },
-  },
+  emmylua_ls = {},
+  ['emmylua-codeformat'] = {}
 }
 
 vim.pack.add {
-  gh 'neovim/nvim-lspconfig',
-  gh 'mason-org/mason.nvim',
-  gh 'mason-org/mason-lspconfig.nvim',
-  gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
+  gh 'neovim/nvim-lspconfig', gh 'mason-org/mason.nvim', gh 'mason-org/mason-lspconfig.nvim',
+  gh 'WhoIsSethDaniel/mason-tool-installer.nvim'
 }
 
 -- Automatically install LSPs and related tools to stdpath for Neovim
